79357b2af825bc99644bb40b7097409eb7f6c441
PiedroCraft <88720549+PiedroCraft@users.noreply.github.com>
2022-05-09 14:44:12 -0300

    sc-38-criar-rodape

---
+++ componets/FooterComponet.vue
@@ -0,0 +1,28 @@
+<template>
+    <footer>
+        <div class="left">
+            <p>Marketplace &copf; Todos os direitos reservados.</p>
+        </div>
+        <div class="right">
+            <p>Mantenha-se conectado:</p>
+            <ul>
+                <li><a href=""><i class="fa-brands fa-facebook-f"></i></a></li>
+                <li><a href=""><i class="fab fa-twitter"></i></a></li>
+                <li><a href=""><i class="fab fa-instagram"></i></a></li>
+            </ul>
+        </div>
+    </footer>
+</template>
+<script>
+export default {
+    name: "FooterComponet",
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+<style lang="scss">
+    @import 'assets/scss/footer.scss';
+</style>
---
+++ pages/home.vue
@@ -0,0 +1,15 @@
+<template>
+    <FooterComponet></FooterComponet>
+</template>
+<script>
+import FooterComponet from "@/componets/FooterComponet.vue";
+export default {
+    name: "HomePage",
+    components: { FooterComponet },
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
