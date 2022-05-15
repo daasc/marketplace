8333566765804e45f900339394c9aedfdf46dcd6
Rian579 <rian.botelho100@gml.com>
2022-05-15 14:16:03 -0300

    adicionando navbar componente

---
+++ components/NavBarComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>div</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'NavBarComponent',
+}
+</script>
+<style scoped></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
-    <!-- deve add um navbar aqui -->
+    <nav-bar-component></nav-bar-component>
     <Nuxt />
     <footer-component></footer-component>
   </div>
 </template>
 <script>
+import NavBarComponent from '~/components/NavBarComponent.vue'
 import FooterComponent from '~/components/FooterComponent.vue'
 export default {
   name: 'DefaultLayout',
-  components: { FooterComponent },
+  components: { FooterComponent, 'nav-bar-component': NavBarComponent },
 }
 </script>
 <style lang="scss" scoped></style>
