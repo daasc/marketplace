0d09da683a6bddf85fbc1ddf6fb8d1b3b212ab8c
daasc <paulojhol@gmail.com>
2022-06-03 11:59:51 -0300

    add list of the categories

--- components/CardCategory.vue
+++ components/CardCategory.vue
@@ -1,27 +1,36 @@
 <template>
   <div class="card-category">
-    <img
-      src="https://i0.wp.com/demo4.drfuri.com/farmart2/wp-content/uploads/sites/11/2021/05/homepage-new-cat-1.png?resize=100%2C86&ssl=1"
-      alt=""
-    />
+    <img :src="urlImage" alt="" />
     <div class="card-name-category">description</div>
   </div>
 </template>
 <script>
 export default {
   name: 'CardCategory',
+  props: {
+    urlImage: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
 }
 </script>
 <style lang="scss" scoped>
 .card-category {
   background-color: #f7f7f6;
   border-radius: 10px;
-  width: 210px;
+  min-width: 190px;
+  height: 210px;
   cursor: pointer;
   padding: 46px 27px 38px;
   display: flex;
   flex-direction: column;
   justify-content: center;
+  margin: 10px 0px;
   &:hover {
     box-shadow: 0 30px 40px 0 rgb(131 132 135 / 15%);
     transition: 0.5s;
