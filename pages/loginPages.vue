7fee121ac307d0d958eb38b34057ebc9b8aad015
luyz0880 <luyz0880@gmail.com>
2022-06-16 19:43:19 -0300

    your msg

--- pages/loginPages.vue
+++ pages/loginPages.vue
@@ -1,79 +1,171 @@
 <template>
-  <div class="Login">
-    <h1>Login</h1>
-    <br />
-    <input type="text" placeholder="Name" required />
-    <br />
-    <br />
-    <input type="email" placeholder="Email" required />
-    <br />
-    <br />
-    <div class="Bt">
-      <button>Log In</button>
+  <form>
+    <div class="Login">
+      <h1>LOG IN</h1>
+      <br />
+      <hr class="hr1" />
+      <br />
+
+      <p>Log In To Your Account</p>
+      <br />
+      <br />
+
+      <input type="text" placeholder="Username or email adress" required />
+      <br />
+      <br />
+
+      <input type="email" placeholder="Password" required />
+      <br />
+      <br />
+
+      <div class="um">
+        <input type="checkbox" checked />
+        <label>Remember to me</label>
+      </div>
+
+      <div class="Bt">
+        <button>Log In</button>
+      </div>
+      <br />
+
+      <section>
+        <div class="linhaComTexto">
+          <span>Or Login Widht</span>
+        </div>
+      </section>
+      <br />
+
+      <div class="social">
+        <button style="color: grey">
+          <i class="fa-brands fa-google"></i
+          >&ensp;&ensp;&ensp;&ensp;&emsp;&emsp;Continue With
+          <b style="font-size: 18px">Google</b>
+        </button>
+      </div>
     </div>
-    <br />
-    <section>
-      <p style="text-align: center">Or login width</p>
-    </section>
-    <br />
-    <div class="social">
-      <i class="fa-brands fa-google"></i
-      ><a style="text-decoration: none" href="#"
-        >Sing In <b style="color: black">Google</b></a
-      >
-    </div>
-  </div>
+  </form>
 </template>
 
 <script></script>
 
 <style scoped>
-.Login h1 {
-  text-align: center;
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-size: 20px;
 }
 
 .Login {
-  margin: 150px;
-  margin-left: 35%;
-  border-radius: 8px;
-  border: solid rgb(202, 201, 201);
-  background-color: rgb(239, 236, 236);
-  height: 180px;
-  padding: 50px;
-  margin-right: 35%;
-}
-
-.Login input {
+  background-color: rgb(237, 233, 233);
+  border: 1px solid rgb(252, 248, 248);
+  width: 28%;
   text-align: center;
-  width: 80%;
-  margin-left: 7%;
+  height: 50%;
+  padding: 90px 0;
+  position: relative;
+  left: 35%;
 }
 
-.social {
-  border-radius: 2px;
-  border: solid rgb(211, 208, 208) 1px;
+.Login h1 {
   text-align: center;
-  background-color: rgb(173, 170, 170);
-  width: 80%;
-  margin-left: 7%;
-  height: 20px;
-}
-
-.social a {
-  color: rgb(255, 255, 255);
-}
-
-.Bt button {
-  cursor: pointer;
-  background-color: rgb(255, 243, 8);
-  width: 50%;
-  margin-left: 25%;
-  border-radius: 2px;
-  border: solid 1px yellow;
-  border-radius: 5px;
+  font-size: 25px;
 }
 
 .Login p {
-  text-decoration: overline;
+  position: absolute;
+  left: 30px;
+  font-size: 15px;
+}
+
+.hr1 {
+  border: 0;
+  height: 2px;
+  background-image: linear-gradient(
+    to right,
+    transparent,
+    rgb(219, 216, 216),
+    transparent
+  );
+}
+
+::-webkit-input-placeholder {
+  font-size: 10px;
+  position: absolute;
+  top: 10px;
+  left: 10px;
+}
+
+.Login input {
+  font-size: 25px;
+  border: 1px solid rgb(216, 213, 213);
+  border-radius: 3px;
+  width: 82%;
+}
+
+.um {
+  position: relative;
+  padding-left: 10px;
+  display: block;
+  right: 140px;
+  margin-bottom: 8px;
+  cursor: pointer;
+}
+
+.um label {
+  padding-left: 20px;
+  position: absolute;
+  right: 70px;
+  font-size: 12px;
+  bottom: 4px;
+}
+
+.Bt button {
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  font-size: 15px;
+  color: black;
+  width: 85%;
+  height: 40px;
+  cursor: pointer;
+  background-color: rgb(221, 181, 3);
+  border: 1px solid rgb(221, 181, 3);
+  border-radius: 2px;
+  transition: 0.5s;
+}
+
+.Bt button:hover {
+  background-color: rgb(229, 206, 0);
+}
+
+.linhaComTexto {
+  width: 100%;
+  height: 2px;
+  text-align: center;
+  background-image: linear-gradient(to right, transparent, #ccc, transparent);
+}
+
+.linhaComTexto > span {
+  position: relative;
+  bottom: 10px;
+  font-size: 15px;
+  background-color: rgb(237, 233, 233);
+  padding: 0 10px;
+}
+
+.social button {
+  width: 80%;
+  font-size: 15px;
+  height: 35px;
+  color: #ccc;
+  cursor: pointer;
+  border: solid 0px;
+  box-shadow: 10px;
+}
+
+@media screen and (max-width: 1000px) {
+  .Login {
+    width: 50%;
+    margin-bottom: 20px;
+  }
 }
 </style>
