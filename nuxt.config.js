8a3f966fadbe306c2c9c5e58be73ac242aa6272b
daasc <paulojhol@gmail.com>
2022-06-19 14:17:06 -0300

    add request product in vuex with actions

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: 'http://localhost:5001/teste-72112/us-central1/api',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
