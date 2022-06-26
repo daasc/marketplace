9a6f6c4c84d5532e60bd397f5f78892d01213874
daasc <paulojhol@gmail.com>
2022-06-26 20:03:33 -0300

    fix css

--- pages/wish/list.vue
+++ pages/wish/list.vue
@@ -1,64 +1,66 @@
 <template>
   <main class="wish-list main">
     <h2>Wishlist</h2>
-    <table class="whish-list-table">
-      <thead>
-        <tr>
-          <th>
-            <span class="nobr"> </span>
-          </th>
-          <th></th>
-          <th>
-            <span class="nobr"> Product name </span>
-          </th>
-          <th>
-            <span class="nobr"> Unit price </span>
-          </th>
-          <th>
-            <span class="nobr"> Stock status </span>
-          </th>
-          <th>
-            <span class="nobr"> </span>
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(item, index) in products" :key="index">
-          <td>
-            <div class="trash">
-              <i
-                class="fa-solid fa-trash-can"
-                @click="removeToWishList(item.id)"
-              ></i>
-            </div>
-          </td>
-          <td>
-            <img
-              width="120"
-              height="120"
-              :src="item.urlImage"
-              alt="product's image"
-            />
-          </td>
-          <td>{{ item.name }}</td>
-          <td class="product-price">${{ item.price }}</td>
-          <td>
-            <span v-if="item.stock > 0" class="in-stock">In Stock</span>
-            <span v-else class="out-of-stock">Out Of Stock</span>
-          </td>
-          <td class="product-add-to-cart">
-            <button class="button-add-product" role="button">
-              Add to cart
-            </button>
-          </td>
-        </tr>
-        <tr v-if="!products.length">
-          <td class="no-product">
-            <span>No products added to the wishlist</span>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="content-table">
+      <table class="whish-list-table">
+        <thead>
+          <tr>
+            <th>
+              <span class="nobr"> </span>
+            </th>
+            <th></th>
+            <th>
+              <span class="nobr"> Product name </span>
+            </th>
+            <th>
+              <span class="nobr"> Unit price </span>
+            </th>
+            <th>
+              <span class="nobr"> Stock status </span>
+            </th>
+            <th>
+              <span class="nobr"> </span>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in products" :key="index">
+            <td>
+              <div class="trash">
+                <i
+                  class="fa-solid fa-trash-can"
+                  @click="removeToWishList(item.id)"
+                ></i>
+              </div>
+            </td>
+            <td>
+              <img
+                width="120"
+                height="120"
+                :src="item.urlImage"
+                alt="product's image"
+              />
+            </td>
+            <td>{{ item.name }}</td>
+            <td class="product-price">${{ item.price }}</td>
+            <td>
+              <span v-if="item.stock > 0" class="in-stock">In Stock</span>
+              <span v-else class="out-of-stock">Out Of Stock</span>
+            </td>
+            <td class="product-add-to-cart">
+              <button class="button-add-product" role="button">
+                Add to cart
+              </button>
+            </td>
+          </tr>
+          <tr v-if="!products.length">
+            <td class="no-product">
+              <span>No products added to the wishlist</span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </main>
 </template>
 <script>
@@ -91,56 +93,62 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.main {
+  margin-top: 100px;
+}
 .wish-list {
-  h2 {
-    text-align: center;
-  }
-  table {
-    width: 80%;
-    margin: 0 auto;
-    &.whish-list-table {
-      font-size: 100%;
-      border-color: #e5e5e5;
-      margin-bottom: 26px;
-      thead {
-        th {
-          background-color: #f5f5f5;
-          font-weight: 400;
-          color: #555;
-          border-color: #eeeeee;
-          padding: 11px 30px;
-        }
-      }
-      tbody {
-        tr {
-          .no-product {
-            padding: 20px;
+  .content-table {
+    overflow-x: auto; /* Horizontal */
+    table {
+      width: 80%;
+      margin: 0 auto;
+      &.whish-list-table {
+        font-size: 100%;
+        border-color: #e5e5e5;
+        margin-bottom: 26px;
+        thead {
+          th {
+            background-color: #f5f5f5;
+            font-weight: 400;
+            color: #555;
+            border-color: #eeeeee;
+            padding: 11px 30px;
           }
-          td {
-            border-bottom: 1px solid #eee;
-            text-align: center;
-            .trash {
-              i {
-                font-size: 25px;
-                cursor: pointer;
+        }
+        tbody {
+          tr {
+            .no-product {
+              padding: 20px;
+            }
+            td {
+              border-bottom: 1px solid #eee;
+              text-align: center;
+              .trash {
+                i {
+                  font-size: 25px;
+                  cursor: pointer;
+                }
               }
-            }
-            .in-stock {
-              color: #2f8a33;
-              font-weight: bold;
-            }
-            .out-of-stock {
-              color: #e31e0a;
-              font-weight: bold;
-            }
-            &.product-price {
-              color: #2f8a33;
-              font-weight: bold;
+              .in-stock {
+                color: #2f8a33;
+                font-weight: bold;
+              }
+              .out-of-stock {
+                color: #e31e0a;
+                font-weight: bold;
+              }
+              &.product-price {
+                color: #2f8a33;
+                font-weight: bold;
+              }
             }
           }
         }
       }
     }
+  }
+  h2 {
+    text-align: center;
   }
 }
 </style>
